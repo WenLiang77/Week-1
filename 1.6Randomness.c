e947a9bbb69b5e9ce47d698a5b57e400a3ec1b1f
WenLiang77 <fd21102@bristol.ac.uk>
2025-09-23 16:43:57 +0100

    Add files via upload

---
+++ 1.6Randomness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int plus_cnt = 0, minus_cnt = 0;
+    int median = RAND_MAX / 2;
+    srand(time(NULL)); // Seed random number generator
+
+    for (int i = 0; i < 500; i++) {
+        int r = rand();
+        if (r < median)
+            minus_cnt++;
+        else
+            plus_cnt++;
+        printf("%d\n", plus_cnt - minus_cnt);
+    }
+
+    return 0;
+}
