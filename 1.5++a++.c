fc541f658eca53cc136e244f9b22800d32f59154
WenLiang77 <fd21102@bristol.ac.uk>
2025-09-23 10:48:44 +0100

    Add files via upload

--- 1.5++a++.c
+++ 1.5++a++.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     int a,b=0, c=0;
-a = ++b + ++c;
-printf("%i %i %i\n",a,b,c);
-a = b++ + c++;
-printf("%i %i %i\n",a,b,c);
-a = ++b + c++;
-printf("%i %i %i\n",a,b,c);
-a = b-- + --c;
-printf("%i %i %i\n",a,b,c);
-return 0;
+    a = ++b + ++c;
+    printf("%i %i %i\n",a,b,c);
+    a = b++ + c++;
+    printf("%i %i %i\n",a,b,c);
+    a = ++b + c++;
+    printf("%i %i %i\n",a,b,c);
+    a = b-- + --c;
+    printf("%i %i %i\n",a,b,c);
+    return 0;
 }
